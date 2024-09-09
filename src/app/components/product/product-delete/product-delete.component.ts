cf90a7cb8cdb9f384c10da7f4277ae2a54b97d99
danieldeclink <daniel@declink.com.br>
2024-09-09 09:08:57 -0300

    Atualização do delete

--- src/app/components/product/product-delete/product-delete.component.ts
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -12,14 +12,22 @@ export class ProductDeleteComponent implements OnInit {
   product: Product | undefined
   constructor(private productService: ProductService ) {}
   ngOnInit(): void {
-    const id = 1;
+    const id = "1";
     this.productService.readById(id).subscribe(product =>{
       this.product = product
     })
   }
 
   deleteProduct(): void{
-
+    if (this.product) {
+      this.productService.delete(this.product).subscribe(() => {
+        this.productService.showMessage('Produto excluido com sucesso');
+        
+      });
+    } else {
+      this.productService.showMessage('Erro: Produto não encontrado');
+      // Outra ação, como redirecionar ou logar o erro
+    }
   }
 
   cancel(): void{
